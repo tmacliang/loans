91b266800f2280da2c27d0cc78922194b10b7ac5
zoro <lcltamc@163.com>
2017-10-25 18:19:41 +0800

    Payment

--- loans-core/src/main/java/org/kelex/loans/core/service/PaymentOrderService.java
+++ loans-core/src/main/java/org/kelex/loans/core/service/PaymentOrderService.java
@@ -6,20 +6,17 @@ import org.kelex.loans.bean.PaymentOrderRequest;
 import org.kelex.loans.core.ServerRuntimeException;
 import org.kelex.loans.core.context.TransactionRequestContext;
 import org.kelex.loans.core.dto.RequestDTO;
-import org.kelex.loans.core.entity.AccountEntity;
-import org.kelex.loans.core.entity.CurrProcessCtrlEntity;
-import org.kelex.loans.core.entity.PaymentProcessCtrlEntity;
-import org.kelex.loans.core.entity.TxnSummaryEntity;
-import org.kelex.loans.core.repository.AccountRepository;
-import org.kelex.loans.core.repository.CycleSummaryRepository;
-import org.kelex.loans.core.repository.RepositoryProxy;
-import org.kelex.loans.core.repository.TxnSummaryRepository;
+import org.kelex.loans.core.entity.*;
+import org.kelex.loans.core.repository.*;
 import org.kelex.loans.core.util.AssertUtils;
+import org.kelex.loans.enumeration.PaymentStatusCodeEnum;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Objects;
 import java.util.Optional;
 
 import static java.math.BigDecimal.ZERO;
@@ -42,6 +39,9 @@ public class PaymentOrderService extends TransactionService<PaymentOrderRequest>
     @Inject
     private TxnSummaryRepository txnSummaryRepository;
 
+    @Inject
+    private PaymentHistoryRepository paymentHistoryRepository;
+
     @Override
     protected void checkArguments(TransactionRequestContext<? extends PaymentOrderRequest> context) throws Exception {
         RequestDTO<? extends PaymentOrderRequest> request = context.getRequest();
@@ -62,36 +62,65 @@ public class PaymentOrderService extends TransactionService<PaymentOrderRequest>
         PaymentOrderRequest data = request.getData();
         //幂等性验证
         Optional<TxnSummaryEntity> txnSummaryOpt = txnSummaryRepository.findOneByOrderNo(data.getPaymentOrderNo());
-        if(txnSummaryOpt.isPresent()){
-            throw new ServerRuntimeException(500,"duplicate Exception");
+        if (txnSummaryOpt.isPresent()) {
+            throw new ServerRuntimeException(500, "duplicate Exception");
         }
 
+        Optional<PaymentHistoryEntity> paymentHistoryOpt = paymentHistoryRepository.findOneByPaymentOrderNo(data.getPaymentOrderNo());
+        if (!paymentHistoryOpt.isPresent()) {
+            throw new ServerRuntimeException(500, "paymentOrderNo not find PaymentHistory");
+        }
+        PaymentHistoryEntity paymentHistory = paymentHistoryOpt.get();
+        if (Objects.equals(paymentHistory.getStatusCode(), data.getPaymentStatusCode())) {
+            throw new ServerRuntimeException(500, "error");
+        }
+        if (Objects.equals(paymentHistory.getAccountId(), data.getAccountId())) {
+            throw new ServerRuntimeException(500, "error");
+        }
+   /*     if (paymentHistory.getPaymentAmt().compareTo(data.getPaymentAmount()) != 0) {
+            throw new ServerRuntimeException(500, "error");
+        }*/
+
+        AccountEntity accountEntity = accountRepository.findOne(data.getAccountId());
+        CycleSummaryId summaryId = new CycleSummaryId();
+        summaryId.setAccountId(accountEntity.getAccountId());
+        summaryId.setCycleNo(accountEntity.getCurrentCycleNo());
+        CycleSummaryEntity cycleSummaryEntity = cycleSummaryRepository.findOne(summaryId);
+
+        context.setAttribute(AccountEntity.class, accountEntity);
+        context.setAttribute(CycleSummaryEntity.class, cycleSummaryEntity);
+        context.setAttribute(PaymentHistoryEntity.class, paymentHistory);
 
     }
 
+
     @Override
     protected void doTransaction(TransactionRequestContext<? extends PaymentOrderRequest> context) throws Exception {
+
+        RequestDTO<? extends PaymentOrderRequest> request = context.getRequest();
+        PaymentOrderRequest data = request.getData();
+
+        RepositoryProxy repositoryProxy = context.getRepository();
         AccountEntity account = (AccountEntity) context.getAttribute(AccountEntity.class);
         CurrProcessCtrlEntity currCtrl = (CurrProcessCtrlEntity) context.getAttribute(CurrProcessCtrlEntity.class);
         PaymentProcessCtrlEntity payCtrl = (PaymentProcessCtrlEntity) context.getAttribute(PaymentProcessCtrlEntity.class);
 
-        RequestDTO<? extends PaymentOrderRequest> request = (RequestDTO<? extends PaymentOrderRequest>) context.getAttribute(RequestDTO.class);
         LocalDate businessDate = request.getBusinessDate();
-
-        PaymentOrderRequest data = request.getData();
         BigDecimal amount = data.getPaymentAmount().multiply(BigDecimal.ONE.scaleByPowerOfTen(currCtrl.getPower()));
 
-//        PaymentHistoryEntity paymentHistory = new PaymentHistoryEntity();
-//        paymentHistory.setProductId(account.getProductId());
-//        paymentHistory.setAccountId(account.getAccountId());
-//        paymentHistory.setCustomerId(account.getCustomerId());
-//        paymentHistory.setCurrencyCode(data.getCurrencyCode());
-//        paymentHistory.setPaymentAmt(amount);
-//        paymentHistory.setIouId(0L);
-//        paymentHistory.setPaymentAmt(data.getPaymentAmount());
-//        paymentHistory.setRequestDate(businessDate);
-//        paymentHistory.setRequestTime(LocalTime.now());
-//        RepositoryProxy repository = context.getRepository();
+        PaymentHistoryEntity paymentHistory = (PaymentHistoryEntity) context.getAttribute(PaymentHistoryEntity.class);
+        if (Objects.equals(data.getPaymentStatusCode(), PaymentStatusCodeEnum.SUCC.toString())) {
+            paymentHistory.setStatusCode(PaymentStatusCodeEnum.SUCC.toString());
+            paymentHistory.setCompleteDate(LocalDate.now());
+            paymentHistory.setResponseDate(businessDate);
+            paymentHistory.setResponseTime(LocalTime.now());
+        } else if (Objects.equals(data.getPaymentStatusCode(), PaymentStatusCodeEnum.FAIL.toString())) {
+            paymentHistory.setStatusCode(PaymentStatusCodeEnum.FAIL.toString());
+            paymentHistory.setCompleteDate(LocalDate.now());
+            paymentHistory.setResponseDate(businessDate);
+            paymentHistory.setResponseTime(LocalTime.now());
+        }
+        repositoryProxy.save(paymentHistory);
         this.hashCode();
     }
 }
