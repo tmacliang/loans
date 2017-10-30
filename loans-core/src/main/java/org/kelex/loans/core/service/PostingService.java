c398c64a12dde32222aa1e530274f71fc3fd86e7
zoro <lclawrb@163.com>
2017-10-30 18:23:15 +0800

    update the balcomval

--- loans-core/src/main/java/org/kelex/loans/core/service/PostingService.java
+++ loans-core/src/main/java/org/kelex/loans/core/service/PostingService.java
@@ -5,6 +5,7 @@ import org.kelex.loans.core.SysintrException;
 import org.kelex.loans.core.context.TransactionContext;
 import org.kelex.loans.core.dto.RequestDTO;
 import org.kelex.loans.core.entity.*;
+import org.kelex.loans.core.enumeration.FlowType;
 import org.kelex.loans.core.repository.*;
 import org.kelex.loans.core.util.TransactionUtils;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 import javax.inject.Inject;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -55,31 +57,44 @@ public class PostingService {
         }
     }
 
+    /**
+     * 执行一个交易的相关更新操作
+     * @param txn
+     * @param context
+     */
     private void postOneTxn(TxnSummaryEntity txn, TransactionContext context) {
-        RepositoryProxy repository = context.getRepository();
 
+        doPosting(txn, context);
+        //更新账户
+        updateAccount(txn, context);
+        //更新额度信息
+        updateCreditLimit(txn, context);
+        //更新账期表
+        updateCycleSummary(txn, context);
+    }
+
+    private void doPosting(TxnSummaryEntity txn, TransactionContext context) {
+        RepositoryProxy repository = context.getRepository();
         TxnSummaryId txnId = txn.getId();
         AccountEntity acccount = accountRepository.findOne(txnId.getAccountId());
-
         CycleSummaryId cycleId = acccount.getCycleId();
-        CycleSummaryEntity cycle = cycleSummaryRepository.findOne(cycleId);
-
+//        CycleSummaryEntity cycle = cycleSummaryRepository.findOne(cycleId);
         List<BalCompValEntity> balCompValList = balCompValRepository.findAll(cycleId.getAccountId(), cycleId.getCycleNo());
         Map<String, BalCompValEntity> bcvMap = TransactionUtils.toMap(balCompValList);
+        context.setAttribute("bcvMap", bcvMap);
+
 
         String productId = txn.getProductId();
         String actTypeId = txn.getActTypeId();
         TxnProfileEntity txnProfile = entryService.findOneTxnProfile(txn.getTxnCode(), repository);
-
         TxnProcessCtrlEntity txnCtrl = entryService.findOneTxnProcessCtrl(productId, actTypeId, txn.getTxnCode(), repository);
         if (txnCtrl == null) {
             throw new SysintrException("txn ctrl is no such: " + productId + ", " + actTypeId + ", " + ", " + txn.getTxnCode());
         }
 
         BigDecimal residueAmt = txn.getPostingAmt();
-
-        String initBcpId = txnCtrl.getInitBcpId();
-        /*if (initBcpId != null) {
+        /*String initBcpId = txnCtrl.getInitBcpId();
+        if (initBcpId != null) {
             BalProcessCtrlEntity initBalCtrl = entryService.findOneBalProcessCtrl(productId, actTypeId, initBcpId, repository);
             if (initBalCtrl == null) {
                 throw new SysintrException("txn init bal ctrl is no such: " + productId + ", " + actTypeId + ", " + initBcpId);
@@ -99,43 +114,117 @@ public class PostingService {
         if (bcp == null) {
             throw new SysintrException("txn bal profile is no such: " + bcpId);
         }
-
-        BalCompValEntity bcv = bcvMap.get(bcpId);
-        if (bcv == null) {
-            BalCompValId newBcvId = new BalCompValId();
-            newBcvId.setAccountId(acccount.getAccountId());
-            newBcvId.setCycleNo(acccount.getCurrentCycleNo());
-            newBcvId.setBcpId(bcpId);
-
-            BalCompValEntity newBcv = new BalCompValEntity();
-            newBcv.setId(newBcvId);
-            newBcv.setCurrencyCodeEnum(txn.getCurrencyCode());
-            newBcv.setFlowType(bcp.getFlowType());
-            newBcv.setBalType(bcp.getBalType());
-            newBcv.setCtdBalance(BigDecimal.ZERO);
-            newBcv.setPvsBalance(BigDecimal.ZERO);
-            newBcv.setOldBalance(BigDecimal.ZERO);
-            bcv = newBcv;
-            bcvMap.put(bcpId, bcv);
+        if (Objects.equals(txnProfile.getFlowType(), FlowType.D)) {
+            BigDecimal creditBal = acccount.getCurrentBalance();
+            if (txnCtrl.getInitBcpId() != null && creditBal.compareTo(ZERO) < 0) {
+                determineApportionOrder(txn, context);
+            }
+//            if (creditBal.compareTo(ZERO) > 0) {
+            depositDebitTxn(residueAmt, bcpId, context);
+//            }
+        } else {
+//            if (creditBal.compareTo(ZERO) > 0) {
+            depositCreditTxn(residueAmt, bcpId, context);
         }
 
-        bcv.setCtdBalance(bcv.getCtdBalance().add(residueAmt));
-
-        repository.save(bcv);
-
-        //TODO:wait
-        this.hashCode();
-
-
-        //更新账户
-        updateAccount(txn, context);
-        //更新额度信息
-        updateCreditLimit(txn, context);
-        //更新账期表
-        updateCycleSummary(txn, context);
-
-
     }
+
+    /**
+     * 决定分配顺序 默认o p c
+     *
+     * @param txnSummary
+     * @param context
+     */
+    private void determineApportionOrder(TxnSummaryEntity txnSummary, TransactionContext context) {
+        char[] orders = new char[]{'O', 'P', 'C'};
+        context.setAttribute("apportionOrder", orders);
+    }
+
+
+    /**
+     * 创建余额成分
+     *
+     * @param bcpId
+     * @param context
+     * @return
+     */
+    public BalCompValEntity createBalCompVal(String bcpId, TransactionContext context) {
+
+        RepositoryProxy repository = context.getRepository();
+        AccountEntity account = (AccountEntity) context.getAttribute(AccountEntity.class);
+        BalCompProfileEntity bcp = entryService.findOneBalCompProfile(bcpId, repository);
+
+        BalCompValId newBcvId = new BalCompValId();
+        newBcvId.setAccountId(account.getAccountId());
+        newBcvId.setCycleNo(account.getCurrentCycleNo());
+        newBcvId.setBcpId(bcpId);
+
+        BalCompValEntity newBcv = new BalCompValEntity();
+        newBcv.setId(newBcvId);
+        newBcv.setFlowType(bcp.getFlowType());
+        newBcv.setBalType(bcp.getBalType());
+        newBcv.setCtdBalance(BigDecimal.ZERO);
+        newBcv.setPvsBalance(BigDecimal.ZERO);
+        newBcv.setOldBalance(BigDecimal.ZERO);
+        return newBcv;
+    }
+
+    /**
+     * 分配借记消费
+     *
+     * @param residueAmt
+     * @param bcpId
+     * @param context
+     */
+    public void depositDebitTxn(BigDecimal residueAmt, String bcpId, TransactionContext context) {
+
+        BalCompProfileEntity balCompProfile = (BalCompProfileEntity) context.getAttribute(BalCompValEntity.class);
+
+        StringBuilder builder = (StringBuilder) context.getAttribute("apportionInfo");
+        if (builder == null) {
+            builder = new StringBuilder(120).append("P#").append(bcpId).append(':');
+        } else {
+            builder.append(',').append(bcpId).append(':');
+        }
+        Map<String, BalCompValEntity> bcvMap = (Map<String, BalCompValEntity>) context.getAttribute("bcvMap");
+        BalCompValEntity bcv = bcvMap.get(bcpId);
+        if (bcv == null) {
+            bcv = createBalCompVal(bcpId, context);
+        }
+        builder.append("C[").append(residueAmt).append("]");
+        context.setAttribute("apportionInfo", builder);
+        bcv.setCtdBalance(bcv.getCtdBalance().add(residueAmt));
+        bcvMap.put(bcpId, bcv);
+    }
+
+
+    /**
+     * 分配贷记消费
+     *
+     * @param residueAmt
+     * @param bcpId
+     * @param context
+     */
+    public void depositCreditTxn(BigDecimal residueAmt, String bcpId, TransactionContext context) {
+
+        StringBuilder builder = (StringBuilder) context.getAttribute("apportionInfo");
+        if (builder == null) {
+            builder = new StringBuilder(120).append("P#").append(bcpId).append(':');
+        } else {
+            builder.append(',').append(bcpId).append(':');
+        }
+        Map<String, BalCompValEntity> bcvMap = (Map<String, BalCompValEntity>) context.getAttribute("bcvMap");
+        BalCompValEntity bcv = bcvMap.get(bcpId);
+        if (bcv == null) {
+            bcv = createBalCompVal(bcpId, context);
+        }
+        builder.append("D[").append(residueAmt).append("]");
+        context.setAttribute("apportionInfo", builder);
+        bcv.setCtdBalance(bcv.getCtdBalance().add(residueAmt));
+        bcvMap.put(bcpId, bcv);
+    }
+
+
 
     private BigDecimal apportionDebitTxn(TxnSummaryEntity txn, TxnProfileEntity txnProfile, Map<String, BalCompValEntity> bcvMap, RepositoryProxy repository) {
 
@@ -178,13 +267,16 @@ public class PostingService {
 
         RepositoryProxy repositoryProxy = context.getRepository();
         AccountEntity accountEntity = (AccountEntity) context.getAttribute(AccountEntity.class);
-        BigDecimal postingAmt = ZERO;
-        BigDecimal osgDeductAmt = ZERO;
+        BigDecimal postingAmt;
+        BigDecimal osgDeductAmt;
 
         TxnProfileEntity txnProfile = txnProfileRepository.findOne(txnSummary.getTxnCode());
-        if (Objects.equals(txnProfile.getFlowType(), "C")) {
+        if (Objects.equals(txnProfile.getFlowType(), FlowType.C)) {
             postingAmt = txnSummary.getPostingAmt().negate();
             osgDeductAmt = txnSummary.getOutstandingDeductAmt().negate();
+        } else {
+            postingAmt = txnSummary.getPostingAmt();
+            osgDeductAmt = txnSummary.getOutstandingDeductAmt();
         }
         accountEntity.setCurrentBalance(accountEntity.getCurrentBalance().add(postingAmt));
         accountEntity.setOutstandingTxnAmt(accountEntity.getOutstandingTxnAmt().add(osgDeductAmt));
@@ -209,7 +301,7 @@ public class PostingService {
         ActCreditDataEntity actCreditData = actCreditDataRepository.findOne(accountEntity.getAccountId());
 
         TxnProfileEntity txnProfile = (TxnProfileEntity) context.getAttribute(TxnProfileEntity.class);
-        if (Objects.equals(txnProfile.getFlowType(), "D")) {
+        if (Objects.equals(txnProfile.getFlowType(), FlowType.D)) {
             actCreditData.setTotalPostedAmt(actCreditData.getTotalPostedAmt().add(txnSummary.getPostingAmt()));
             actCreditData.setOutstandingAuthAmt(actCreditData.getOutstandingAuthAmt().add(txnSummary.getOutstandingDeductAmt()));
         } else {
@@ -229,14 +321,13 @@ public class PostingService {
 
     public void updateCycleSummary(TxnSummaryEntity txnSummary, TransactionContext context) {
         CycleSummaryEntity cycleSummary = (CycleSummaryEntity) context.getAttribute(CycleSummaryEntity.class);
-        AccountEntity account = (AccountEntity) context.getAttribute(AccountEntity.class);
         TxnProfileEntity txnProfile = (TxnProfileEntity) context.getAttribute(TxnProfileEntity.class);
 
         LocalDate now = txnSummary.getTxnDate();
         LocalDate dueDate = cycleSummary.getOpenDueDate();
         BigDecimal postingAmt = txnSummary.getPostingAmt();
 
-        if (Objects.equals(txnProfile.getFlowType(), "D")) {
+        if (Objects.equals(txnProfile.getFlowType(), FlowType.D)) {
             //宽限期：信用卡最后还款日之后的几天，bhh出账单到最后还款日中间记在graceAmt上
             if (dueDate != null && !now.isAfter(dueDate)) {
                 cycleSummary.setTotalGraceDebitAmt(cycleSummary.getTotalGraceDebitAmt().add(postingAmt));
